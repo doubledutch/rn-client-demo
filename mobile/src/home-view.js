ff9c2abc5b7e515a9080f28563fb8657728cb271
Adam Liechty <adam@doubledutch.me>
2018-07-17 11:48:48 -0700

    + buttons for new binding functions

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ReactNative, {
-  TouchableOpacity, Text, TextInput, View, ScrollView
+  Alert, Button, Text, View, ScrollView
 } from 'react-native'
 
 import client, { Avatar, TitleBar } from '@doubledutch/rn-client'
@@ -22,6 +22,13 @@ export default class HomeView extends Component {
             <Avatar size={50} user={currentUser} />
             <Text style={{paddingLeft: 10, fontSize: 18}}>{currentUser.firstName} {currentUser.lastName}</Text>
           </View>
+
+          <Button title="getCurrentUser" onPress={() => client.getCurrentUser().then(result => Alert.alert("getCurrentUser", JSON.stringify(result, null, 2)))} />
+          <Button title="getCurrentEvent" onPress={() => client.getCurrentEvent().then(result => Alert.alert("getCurrentEvent", JSON.stringify(result, null, 2)))} />
+          <Button title="logOut" onPress={() => client.logOut()} />
+          <Button title="dismissLandingPage" onPress={() => client.dismissLandingPage(false)} />
+          <Button title="dismissLandingPage(permanent)" onPress={() => client.dismissLandingPage(true)} />
+
           <Text>client.currentUser: {JSON.stringify(currentUser, null, 2)}</Text>
 
           <Text style={{paddingTop: 15, fontSize: 18, textAlign: 'center'}}>{currentEvent.name}</Text>
