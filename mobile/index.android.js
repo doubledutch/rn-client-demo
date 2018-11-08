0610fafb9dd83256b42e986345dfc4472cb41133
Adam Liechty <adam@doubledutch.me>
2018-11-08 14:39:53 -0800

    update xcproj

--- mobile/index.android.js
+++ mobile/index.android.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native'
 import HomeView from './src/home-view'
 
+console.ignoredYellowBox = ['Warning: View.propTypes has been deprecated']
 AppRegistry.registerComponent('clientdemo', () => HomeView)
